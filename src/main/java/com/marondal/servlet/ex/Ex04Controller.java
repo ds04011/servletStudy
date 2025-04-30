34a28c71117369858d5f65503fbe4c16943ff4b2
ds04011 <tommyhw97@gmail.com>
2025-04-30 18:45:22 +0900

    ds04011 -Post method 연습중,

--- src/main/java/com/marondal/servlet/ex/Ex04Controller.java
+++ src/main/java/com/marondal/servlet/ex/Ex04Controller.java
@@ -1,5 +1,25 @@
 package com.marondal.servlet.ex;
 
-public class Ex04Controller {
+import java.io.IOException;
+import java.io.PrintWriter;
 
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class Ex04Controller extends HttpServlet{
+	
+	@Override 
+	public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		
+		response.setCharacterEncoding("utf-8");
+		response.setContentType("text/html");
+		
+		PrintWriter out = response.getWriter();
+		
+		//이름이 들어나면 안되겠는데? Post method 로 보내자.
+		
+		
+		
+	}
 }
